d81195fe580754589fbc04901f095420bfee1216
Michael Ngarimu <ngarimu@gmail.com>
2017-11-17 01:40:13 -0800

    - opencv-img.py: remove actual display of the image

--- opencv-img.py
+++ opencv-img.py
@@ -3,5 +3,6 @@ import cv2
 datadir = '/usr/lib/python2.7/dist-packages/skimage/data/'
 
 img = cv2.imread(datadir + 'astronaut.png')
-cv2.imshow('image', img)
-cv2.waitKey()
+# cv2.imshow('image', img)
+# cv2.waitKey()
+
