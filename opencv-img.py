5a8fa05feb6036d96d47b97fd84e0f4b5db5773b
Michael Ngarimu <ngarimu@gmail.com>
2017-11-17 23:11:40 -0800

    - Calculating everything except d4sigma and classification

--- opencv-img.py
+++ opencv-img.py
@@ -1,8 +1,25 @@
 import cv2
 
+cv2.startWindowThread()
+cv2.namedWindow('image')
 datadir = '/usr/lib/python2.7/dist-packages/skimage/data/'
 
 img = cv2.imread(datadir + 'astronaut.png')
-# cv2.imshow('image', img)
-# cv2.waitKey()
+
+# Adding text
+cv2.putText(img,
+            'Some text',
+            (10,10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.4,
+            (255, 255, 255))
+
+cv2.putText(img,
+            'Some more text',
+            (10,20),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.4,
+            (255, 255, 255))
+
+cv2.imshow('image', img)
 
--- opencv-video.py
+++ opencv-video.py
@@ -6,62 +6,22 @@ import time
 def displayResults(img, results):
     textRow = 10
     textRowSize = 20
-    
-    cv2.putText(img,
-                'rate:{rate:.3f}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
-    
-    cv2.putText(img,
-                'mean(G):{green_mean:.3f}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
 
-    cv2.putText(img,
-                'std(G):{green_stddev:.3f}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
-
-    cv2.putText(img,
-                'Centroid(red):{red_centroid}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
-
-    cv2.putText(img,
-                'Centroid(green):{green_centroid}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
-
-    cv2.putText(img,
-                'Centroid(blue):{blue_centroid}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
-
-    cv2.putText(img,
-                'Saturated pixel count:{saturated_pixel_count}'.format(**results),
-                (10,textRow),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.4,
-                (255, 255, 255))
-    textRow += textRowSize
+    # \todo determine where in the image text will appear and select a
+    # color to contrast the image background.
+    #
+    # \todo sort the keys
+    keys = results.keys()
+    keys.sort()
+    for key in keys:
+        cv2.putText(img,
+                    '{}: {}'.format(key, results[key]),
+                    (10,textRow),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.4,
+                    (255, 255, 255))
+        textRow += textRowSize
+        
 
 # cv2.namedWindow('frame')
 # cv2.startWindowThread()
@@ -80,16 +40,18 @@ while cam.isOpened():
     last_ts = this_ts
     sum_acquisition_intervals += delta_ts
     count += 1
-    results['rate'] = count / sum_acquisition_intervals
+    results['7. rate'] = '{:.3f}'.format(count / sum_acquisition_intervals)
 
+    results['1. Image dimensions'] = frame.shape[:2]
+    
     # Split out RGB channels. OpenCV stores pixels in BGR order.
     redChannel = frame[:,:,2]
     greenChannel = frame[:,:,1]
     blueChannel = frame[:,:,0]
 
     # Compute mean and std dev of the pixels in the green channel
-    results['green_mean'] = blueChannel.mean()
-    results['green_stddev'] = blueChannel.std()
+    results['2. mean(green)'] = '{:.3f}'.format(blueChannel.mean())
+    results['3. std(green)'] = '{:.3f}'.format(blueChannel.std())
 
     # Centroid for each channel - while we could use ndarray syntax
     # unrolling here and operating on the specific channels is
@@ -98,15 +60,17 @@ while cam.isOpened():
     def centroid(moments):
         return (int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00']))
     redMoments = cv2.moments(redChannel)
-    results['red_centroid'] = centroid(redMoments)
+    results['4. Centroid(red)'] = centroid(redMoments)
     greenMoments = cv2.moments(greenChannel)
-    results['green_centroid'] = centroid(greenMoments)
+    results['5. Centroid(green)'] = centroid(greenMoments)
     blueMoments = cv2.moments(blueChannel)
-    results['blue_centroid'] = centroid(blueMoments)
+    results['6. Centroid(blue)'] = centroid(blueMoments)
     
     # Count the saturated pixels - any pixel in any channel that is saturated
-    saturated_pixel_count = 0 #len([1 for row in frame for col in range(len(row)) if (row[col] == 255).any()])
-    results['saturated_pixel_count'] = saturated_pixel_count
+    #saturated_pixel_count = len([1 for row in frame for col in range(len(row)) if (row[col] == 255).any()])
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    saturated_pixel_count = (hsv[:,:,1].flatten() == 255).sum()
+    results['8. Saturated Pixel Count'] = saturated_pixel_count
     
     displayResults(frame, results)
 
@@ -114,7 +78,6 @@ while cam.isOpened():
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
-
     
 cam.release()
 cv2.destroyAllWindows()
