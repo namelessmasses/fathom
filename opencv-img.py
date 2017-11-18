3a5e4dce2e6bc50d4d2a071ef660df23d0c00fab
Michael Ngarimu <ngarimu@gmail.com>
2017-11-18 03:36:45 -0800

    - opencv-img.py: processing classification

--- opencv-img.py
+++ opencv-img.py
@@ -1,25 +1,33 @@
 import cv2
 
-cv2.startWindowThread()
-cv2.namedWindow('image')
+# cv2.startWindowThread()
+# cv2.namedWindow('image')
 datadir = '/usr/lib/python2.7/dist-packages/skimage/data/'
+# img_path = datadir + 'horse.png'
+# img = cv2.imread(img_path)
+# print(img.shape[:2])
 
-img = cv2.imread(datadir + 'astronaut.png')
+# cv2.imshow('image', img)
 
-# Adding text
-cv2.putText(img,
-            'Some text',
-            (10,10),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.4,
-            (255, 255, 255))
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
 
-cv2.putText(img,
-            'Some more text',
-            (10,20),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.4,
-            (255, 255, 255))
+model = ResNet50(weights='imagenet')
 
-cv2.imshow('image', img)
+f = 'coffee.png'
+img_path = datadir + f
+img = cv2.imread(img_path)
+print(img.dtype, img.shape)
+x = cv2.resize(img, (224, 224))
+print(x.dtype, x.shape)
+x = np.expand_dims(x, axis=0)
+print(x.dtype, x.shape)
+#    x = preprocess_input(x)
 
+preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+dp = decode_predictions(preds, top=3)[0]
+print('Predicted:', dp)
