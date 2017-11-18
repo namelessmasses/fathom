9d05c60ba907c5181476d2e3db09c4d678fc929d
Michael Ngarimu <ngarimu@gmail.com>
2017-11-17 21:23:01 -0800

    - opencv-video.py: calculating frame capture rate

--- opencv-video.py
+++ opencv-video.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python2
 
 import cv2
+import time
+
+# cv2.namedWindow('frame')
+# cv2.startWindowThread()
 
 cam = cv2.VideoCapture(0)
-while (cam.isOpened()):
+last_ts = time.time()
+sum_acquisition_intervals = 0
+count = 0
+while cam.isOpened():
     ret, frame = cam.read()
+
+    this_ts = time.time()
+    delta_ts = this_ts - last_ts
+    last_ts = this_ts
+    sum_acquisition_intervals += delta_ts
+    count += 1
+    
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
     
+    print('delta_ts: {} avg: {} rate: {}'.format(delta_ts,
+                                                 sum_acquisition_intervals / count,
+                                                 count / sum_acquisition_intervals))
+
 cam.release()
 cv2.destroyAllWindows()
+print('closing')
